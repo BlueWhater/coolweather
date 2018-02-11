e8437c4a312321391f2d8b8cc43e55bfacb6cdd5
Whale <463474838@qq.com>
2018-02-11 11:23:46 +0800

    加入显示天气信息功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by BS on 2018/2/6.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
